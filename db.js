041f8f5c06f18e1082beb086155e7e8b6b60a0c3
Ho Jun <vnvn1590@daum.net>
2019-08-19 09:29:49 +0900

    08.16

---
+++ db.js
@@ -0,0 +1,62 @@
+export const videos = [
+    {
+        id: 2323,
+        title: 'video awesome',
+        description: 'this it sparta',
+        views: 35,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id:121212,
+            name: ":LCK",
+            email: "lck@lck.com"
+        }
+    },
+    {
+        id: 1234,
+        title: 'list action',
+        description: 'now tih is list',
+        views: 634,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id:343,
+            name: ":List",
+            email: "List@List.com"
+        }
+    },
+    {
+        id: 5683,
+        title: 'what is second',
+        description: "what's the mean?",
+        views: 664,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id:615234,
+            name: ":sec",
+            email: "sec@sec.com"
+        }
+    },
+    {
+        id: 76578,
+        title: 'Tor is fighter',
+        description: 'but he is looser',
+        views: 35,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id:121212,
+            name: ":tor",
+            email: "tor@ror.com"
+        }
+    },
+    {
+        id: 75687,
+        title: 'video',
+        description: 'video',
+        views: 3235,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id:97215,
+            name: ":LCK",
+            email: "lck@lck.com"
+        }
+    }
+]
